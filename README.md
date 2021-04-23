406db0fd6058692eb8f1d8785db1277ad9914adb
杨勇 <1994101600@qq.com>
2021-04-23 16:35:36 +0800

    initial commit

--- README.md
+++ README.md
@@ -8,5 +8,23 @@
 
 ## compose
 
-- dev：docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d
+-d 后台执行，--build 会强制重新构建镜像
+- dev：docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d [--build]
+
+通过启动 docker 容器时，同时会启动两个 node-app 集群，配合 nginx 负载均衡.
+
+> 你可能会问：`为什么启动两个 node 应用可以监听相同的 3000 端口？`
+> 这是因为 docker 各个容器都是独立的子网 ip，你可以通过 `docker inspect node-docker_node-app_1` 或 `docker inspect node-docker_node-app_1` 查看 ip address
+
+- dev：docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --scale node-app=2
+
+-V 会重新创建 volume ，而不是使用之前容器的 volume。
+场景比如，此项目 node-app 中，volume 使用了 /app/node_modules，若安装了新的 node_modules 包，应该重新创建 volume，而不是使用之前本地缓存 volume。
+
+- dev：docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d -V
+
 - prod：docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build
+
+> 注意：-v 会删除所有 volume 数据，所以数据库数据会销毁
+- kill dev: docker-compose -f docker-compose.yml -f docker-compose.dev.yml down -v
+- kill prod: docker-compose -f docker-compose.yml -f docker-compose.prod.yml down
--- index.js
+++ index.js
@@ -1,10 +1,87 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const redis = require('redis')
+const cors = require('cors')
+const {
+  MONGO_IP,
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_PROT,
+  REDIS_URL,
+  REDIS_PORT,
+  SESSION_SECRET
+} = require('./config/config')
+
+let RedisStore = require('connect-redis')(session)
+let redisClient = redis.createClient({
+  host: REDIS_URL,
+  port: REDIS_PORT
+})
+
+const postRouter = require('./routes/postRoutes')
+const userRouter = require('./routes/userRoutes')
 
 const app = express()
 
-app.get('/', (req, res) => {
+/**
+ * 查看 docker network 找到 mongo ip
+ * docker inspect <container name>
+ * here is: docker inspect node-docker_mongo_1
+ * 不建议直接使用 ip
+ */
+// mongoose.connect(
+//   'mongodb://yangyong:mypassword@192.168.208.2:27017/?adminSource=admin'
+// ).then(() => console.log('successfully connected to DB'))
+// .catch((e) => console.log(e))
+
+// 推荐直接使用 docker-compose.yml 中 service 容器名称或者实际容器名称（作为 DNS 域名），docker 容器间可通过内部 DNS 服务互相通信
+// https://docs.docker.com/config/containers/container-networking/
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PROT}/?adminSource=admin`
+
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    })
+    .then(() => console.log('successfully connected to DB'))
+    .catch((e) => {
+      console.log(e)
+      setTimeout(connectWithRetry, 5000)
+    })
+}
+
+connectWithRetry()
+
+// 配合 nginx 代理
+app.enable('trust proxy')
+app.use(cors({}))
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: SESSION_SECRET,
+    cookie: {
+      secure: false,
+      resave: false,
+      saveUninitialized: false,
+      httpOnly: true,
+      maxAge: 30000
+    }
+  })
+)
+
+app.use(express.json())
+
+app.get('/api/v1', (req, res) => {
+  console.log('yeah it ran')
   res.send('<h2>Hi There</h2>')
 })
+
+// localhost:3000/api/v1/posts/
+app.use('/api/v1/posts', postRouter)
+app.use('/api/v1/users', userRouter)
 
 const port = process.env.PORT || 3000
 
